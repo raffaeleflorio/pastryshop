0b74836babe241cf0084c4a19a2717e6fa33d965
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-10-02 12:11:40 +0200

    JsonSummary(Test)

---
+++ src/main/java/io/github/raffaeleflorio/pastryshop/domain/pastry/JsonSummary.java
@@ -0,0 +1,36 @@
+package io.github.raffaeleflorio.pastryshop.domain.pastry;
+
+import jakarta.json.JsonObject;
+
+/**
+ * A {@link Summary} that animates a {@link JsonObject}
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @since 1.0.0
+ */
+public final class JsonSummary implements Summary {
+  /**
+   * Builds a summary
+   *
+   * @param origin    The json to animate
+   * @param summaries The summaries where to track/untrack
+   * @since 1.0.0
+   */
+  public JsonSummary(final JsonObject origin, final Summaries summaries) {
+    this.origin = origin;
+    this.summaries = summaries;
+  }
+
+  @Override
+  public void track() {
+    summaries.add(origin);
+  }
+
+  @Override
+  public JsonObject description() {
+    return origin;
+  }
+
+  private final JsonObject origin;
+  private final Summaries summaries;
+}
---
+++ src/test/java/io/github/raffaeleflorio/pastryshop/domain/pastry/JsonSummaryTest.java
@@ -0,0 +1,38 @@
+package io.github.raffaeleflorio.pastryshop.domain.pastry;
+
+import jakarta.json.Json;
+import org.junit.jupiter.api.Test;
+
+import static io.github.raffaeleflorio.pastryshop.hamcrest.IsThrowedWithMessage.throwsWithMessage;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class JsonSummaryTest {
+  @Test
+  void testDescription() {
+    var description = Json.createObjectBuilder()
+      .add("name", "a pastry")
+      .add("price", 42)
+      .add("another characteristic", "a value")
+      .build();
+    assertThat(
+      new JsonSummary(description, new Summaries.Fake()).description(),
+      equalTo(description)
+    );
+  }
+
+  @Test
+  void testTrack() {
+    assertThat(
+      () -> new JsonSummary(
+        Json.createObjectBuilder()
+          .add("name", "pastry name")
+          .add("ingredients", Json.createArrayBuilder().add("first").add("second"))
+          .add("price", 123456789)
+          .build(),
+        new Summaries.Fake()
+      ).track(),
+      throwsWithMessage(IllegalStateException.class, "Unable to add to a fake")
+    );
+  }
+}
