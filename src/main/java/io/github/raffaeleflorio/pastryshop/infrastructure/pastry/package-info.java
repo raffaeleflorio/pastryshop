a9f6711d10674894265e5c6f49409802262b37a9
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-10-02 12:49:37 +0200

    pastry infrastructure package info

---
+++ src/main/java/io/github/raffaeleflorio/pastryshop/infrastructure/pastry/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package about pastries infrastrucutre
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @since 1.0.0
+ */
+package io.github.raffaeleflorio.pastryshop.infrastructure.pastry;
