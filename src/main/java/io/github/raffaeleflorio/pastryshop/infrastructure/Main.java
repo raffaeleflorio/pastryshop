55dd76e1f991e86acfd1f697a7c9c7fd688661fb
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-10-02 13:17:20 +0200

    Main as Spring Boot application

--- src/main/java/io/github/raffaeleflorio/pastryshop/infrastructure/Main.java
+++ src/main/java/io/github/raffaeleflorio/pastryshop/infrastructure/Main.java
@@ -1,13 +1,17 @@
 package io.github.raffaeleflorio.pastryshop.infrastructure;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * The entrpoint
  *
  * @author Raffaele Florio (raffaeleflorio@protonmail.com)
  * @since 1.0.0
  */
-public final class Main {
+@SpringBootApplication
+public class Main {
   public static void main(String[] args) {
-
+    SpringApplication.run(Main.class, args);
   }
 }
