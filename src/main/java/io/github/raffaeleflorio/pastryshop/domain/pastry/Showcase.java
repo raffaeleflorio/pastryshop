152ca28dbaf935312443186daec3babb52e554b2
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-10-03 09:48:51 +0200

    Showcase.Fake(Test)

--- src/main/java/io/github/raffaeleflorio/pastryshop/domain/pastry/Showcase.java
+++ src/main/java/io/github/raffaeleflorio/pastryshop/domain/pastry/Showcase.java
@@ -1,5 +1,6 @@
 package io.github.raffaeleflorio.pastryshop.domain.pastry;
 
+import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
 
@@ -25,4 +26,61 @@ public interface Showcase {
    * @since 1.0.0
    */
   JsonArray description();
+
+  /**
+   * {@link Showcase} validated according some rules
+   *
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  interface Validated extends Showcase {
+
+  }
+
+  /**
+   * {@link Showcase} useful for testing
+   *
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  final class Fake implements Showcase {
+    /**
+     * Builds an empty fake unable to add items
+     *
+     * @since 1.0.0
+     */
+    public Fake() {
+      this(
+        () -> {
+          throw new IllegalStateException("Unable to add to a fake");
+        },
+        Json.createArrayBuilder().build()
+      );
+    }
+
+    /**
+     * Builds a fake
+     *
+     * @param addFn       The add function
+     * @param description The description
+     * @since 1.0.0
+     */
+    public Fake(final Runnable addFn, final JsonArray description) {
+      this.addFn = addFn;
+      this.description = description;
+    }
+
+    @Override
+    public void add(final JsonObject description) {
+      addFn.run();
+    }
+
+    @Override
+    public JsonArray description() {
+      return description;
+    }
+
+    private final Runnable addFn;
+    private final JsonArray description;
+  }
 }
---
+++ src/test/java/io/github/raffaeleflorio/pastryshop/domain/pastry/ShowcaseTest.java
@@ -0,0 +1,31 @@
+package io.github.raffaeleflorio.pastryshop.domain.pastry;
+
+import jakarta.json.Json;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static io.github.raffaeleflorio.pastryshop.hamcrest.IsThrowedWithMessage.throwsWithMessage;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class ShowcaseTest {
+  @Nested
+  class FakeTest {
+    @Test
+    void testDefaultAddingException() {
+      assertThat(
+        () -> new Showcase.Fake().add(Json.createObjectBuilder().build()),
+        throwsWithMessage(IllegalStateException.class, "Unable to add to a fake")
+      );
+    }
+
+    @Test
+    void testDefaultDescription() {
+      var description = Json.createArrayBuilder().build();
+      assertThat(
+        new Showcase.Fake().description(),
+        equalTo(description)
+      );
+    }
+  }
+}
