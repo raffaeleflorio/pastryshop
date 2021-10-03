10f8aac13d356f457801d0a5c8763b28285b9d08
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-10-03 20:12:21 +0200

    removed unused package

--- src/main/java/io/github/raffaeleflorio/pastryshop/infrastructure/pastry/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Package about pastries infrastructure
- *
- * @author Raffaele Florio (raffaeleflorio@protonmail.com)
- * @since 1.0.0
- */
-package io.github.raffaeleflorio.pastryshop.infrastructure.pastry;
